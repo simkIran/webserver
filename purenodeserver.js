5927b1391dd62d5ef0b831f4d78a57a3e6e0a457
Kirandeep Kaur <741453@student.tor.loc>
2019-07-31 14:55:10 -0400

    2:55pm commit

--- purenodeserver.js
+++ purenodeserver.js
@@ -1,8 +1,11 @@
 const http=require('http');
 
 function handler(req,res){
+    console.log(req.url);
     res.writeHead(200,{'Content-type':'text/plain'});
     res.write('Hello, I am a webserver!')
     res.end();
 }
     const server=http.craeteServer(handler);
+
+    server.listen(3000);
